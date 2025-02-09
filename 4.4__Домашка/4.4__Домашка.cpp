90bab0133f944e028a6ecd45da7deb00f8613f4a
Nik <airgit1991@rambler.ru>
2025-02-09 20:39:27 +0300

    1__hw

---
+++ 4.4__Домашка/4.4__Домашка.cpp
@@ -0,0 +1,27 @@
+﻿// 4.4__Домашка.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(0, "");
+    // Задание 1. Минимум из двух чисел
+    int firstNum;
+    int secondNum;
+    std::cout << "Введите первое число: ";
+    std::cin >> firstNum;
+    std::cout << "Введите второе число: ";
+    std::cin >> secondNum;
+
+    if (firstNum > secondNum) {
+        std::cout << "Наименьшее число: " << secondNum << "\n";
+    }
+    else if (firstNum == secondNum) {
+        std::cout << "Числа равны!\n";
+    }
+    else {
+        std::cout << "Наименьшее число: " << firstNum << "\n";
+    }
+}
+
