ed0487d96af70a00b16270c0f28666032773199c
Nik <airgit1991@rambler.ru>
2025-02-15 23:28:33 +0300

    #2

--- 4.4__Домашка/4.4__Домашка.cpp
+++ 4.4__Домашка/4.4__Домашка.cpp
@@ -23,5 +23,25 @@ int main()
     else {
         std::cout << "Наименьшее число: " << firstNum << "\n";
     }
+
+
+    //Задание 2. Складываем в уме
+    int userNumbOne;
+    int userNumbSecond;
+    int userSum;
+    std::cout << "Введите первое число: ";
+    std::cin >> userNumbOne;
+    std::cout << "Введите второе число: ";
+    std::cin >> userNumbSecond;
+    std::cout << "Введите их сумму: ";
+    std::cin >> userSum;
+    int aiSum = userNumbOne + userNumbSecond;
+    std::cout << "-----Проверяем-----" << "\n";
+    if (userSum == aiSum) {
+        std::cout << "Верно!" << "\n";
+    }
+    else {
+        std::cout << "Ошибка! Верный результат:" << aiSum << "\n";
+    }
 }
 
